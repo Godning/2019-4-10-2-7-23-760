84acf5028ab35d7b815fdc2cda89c1e8b5961d64
godning3 <736397792@qq.com>
2019-05-31 10:38:10 +0800

    [update] main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var dis = inputs.distance;
+    var pt = inputs.parkTime;
+    var pre_price = 0.8;
+    var out = dis>8 ? dis-8 : 0;
+    var pt_price = pt * 0.25;
+    var price;
+    if(dis<=2){
+        price = 6 + pt_price;
+    }else{
+        price = 6 + (dis-2) * pre_price + out * pre_price * 0.5 + pt_price;
+    }
+    return Math.round(price);
 };
